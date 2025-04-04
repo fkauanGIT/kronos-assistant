f62fc5c923c7f7fbcd3737398c4407f811fa0c29
fkauanGIT <kauanuniversity@gmail.com>
2025-04-04 11:41:59 -0300

    feat: creating patient service

---
+++ kronos-assistant/src/main/java/com/project/kronos_assistant/service/PatientService.java
@@ -0,0 +1,33 @@
+package com.project.kronos_assistant.service;
+
+import com.project.kronos_assistant.model.Patient;
+import com.project.kronos_assistant.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> getPatientById(Long id) {
+        return patientRepository.findById(id);
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatient(Long id) {
+        patientRepository.deleteById(id);
+    }
+}
